46a70eeed2059200a105eb64f37faa007592e6b6
Ayush Shukla <ayushshukla8920@gmail.com>
2025-08-09 22:50:37 +0530

    v1 final

--- services/file_loader.py
+++ services/file_loader.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import mimetypes
 from services.pdf_loader import extract_text_from_pdf
@@ -7,6 +8,7 @@ from PIL import Image
 import pytesseract
 import io
 import logging
+from docx import Document
 logger = logging.getLogger(__name__)
 def extract_text_from_pptx(file_url: str) -> str:
     response = requests.get(file_url)
@@ -35,26 +37,28 @@ def extract_text_from_image(file_url: str) -> str:
     img = Image.open(io.BytesIO(response.content))
     text = pytesseract.image_to_string(img)
     return text
+def extract_text_from_docx(file_url: str) -> str:
+    response = requests.get(file_url)
+    response.raise_for_status()
+    document = Document(io.BytesIO(response.content))
+    text = []
+    for paragraph in document.paragraphs:
+        text.append(paragraph.text)
+    return "\n".join(text)
 def extract_text_from_file(file_url: str) -> str:
     logger.info(f"Downloading file from {file_url}")
+    url_path = file_url.split('?')[0]
+    file_extension = os.path.splitext(url_path)[1].lower()
     file_type, _ = mimetypes.guess_type(file_url)
-    logger.info(f"Detected file type: {file_type}")
-    if not file_type:
-        if file_url.endswith('.pdf'):
-            file_type = 'application/pdf'
-        elif file_url.endswith('.pptx'):
-            file_type = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
-        elif file_url.endswith('.xlsx'):
-            file_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-        elif file_url.endswith(('.jpeg', '.jpg', '.png')):
-            file_type = 'image/jpeg'
-    if file_type == 'application/pdf':
+    if file_type == 'application/pdf' or file_extension == '.pdf':
         return extract_text_from_pdf(file_url)
-    elif file_type == 'application/vnd.openxmlformats-officedocument.presentationml.presentation':
+    elif file_type == 'application/vnd.openxmlformats-officedocument.presentationml.presentation' or file_extension == '.pptx':
         return extract_text_from_pptx(file_url)
-    elif file_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
+    elif file_type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' or file_extension in ('.docx', '.doc'):
+        return extract_text_from_docx(file_url)
+    elif file_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file_extension in ('.xlsx', '.xls'):
         return extract_text_from_xlsx(file_url)
-    elif file_type and file_type.startswith('image/'):
+    elif file_type and file_type.startswith('image/') or file_extension in ('.jpeg', '.jpg', '.png'):
         return extract_text_from_image(file_url)
     else:
         raise ValueError(f"Unsupported file type: {file_type}")
